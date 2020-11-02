6175d5e583b1b368b9a93fd78964d55044032402
Ricardo Pessoa <ricardoandre.pessoa@gmail.com>
2020-11-02 17:13:18 -0300

    errors

--- client/src/app/photos/photo.module.ts
+++ client/src/app/photos/photo.module.ts
@@ -1,14 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
     declarations: [ 
         PhotoComponent, 
-        PhotoListComponent 
+        PhotoListComponent, 
+        PhotoFormComponent 
     ],
-    imports: [ HttpClientModule ]
+    imports: [
+        HttpClientModule,
+        CommonModule 
+    ]
 })
 export class PhotosModule {}
