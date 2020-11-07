daf944cce576c31272923851f8f500a607b320e7
Ricardo Pessoa <ricardoandre.pessoa@gmail.com>
2020-11-07 20:45:49 -0300

    logout

--- client/src/app/core/user/user.service.ts
+++ client/src/app/core/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { TokenService } from '../token/token.service';
 import { User } from './user';
 import * as jw_decode from 'jwt-decode';
@@ -7,7 +7,8 @@ import * as jw_decode from 'jwt-decode';
 @Injectable({ providedIn: 'root' })
 export class UserService {
 
-    private userSubject = new Subject<User>();
+    private userSubject = new BehaviorSubject<User>(null);
+    private userName: string;
 
     constructor(private tokenService: TokenService) { 
         this.tokenService.hasToken() &&
@@ -26,7 +27,21 @@ export class UserService {
     private decodeAndNotify() {
         const token = this.tokenService.getToken();
         const user = jw_decode(token) as User;
+        this.userName = user.name;
         this.userSubject.next(user);
+    }
+
+    logout() {
+        this.tokenService.removeToken();
+        this.userSubject.next(null);
+    }
+
+    isLogged() {
+        return this.tokenService.hasToken();
+    }
+
+    getUserName() {
+        return this.userName;
     }
    
 }
