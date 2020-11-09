741ce4bf7b4e8a92133629aa47bbe0f68fa1ffd0
Ricardo Pessoa <ricardoandre.pessoa@gmail.com>
2020-11-08 22:15:26 -0300

    Interceptadores

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
-import { CoreModule } from './core/core.modelu';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
