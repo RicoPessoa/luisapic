156ce38035bdae91d78b2b9c774420a23ef60f2b
Ricardo Pessoa <ricardoandre.pessoa@gmail.com>
2020-11-10 14:58:33 -0300

    snapshot

--- client/src/app/photos/photos.module.ts
+++ client/src/app/photos/photos.module.ts
@@ -1,16 +1,20 @@
-import { CommonModule } from '@angular/common';
+
 import { NgModule } from '@angular/core';
 
 import { PhotoModule } from './photo/photo.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoDetailsModule } from './photo-details/photo-details.module';
+import { DarkenOnHoverModule } from '../shared/directives/darken-on-hover/darken-on-hover.module';
+
 
 @NgModule({
     imports: [
         PhotoModule,
-        PhotoListModule,
         PhotoFormModule,
-        CommonModule
+        PhotoListModule,
+        DarkenOnHoverModule,
+        PhotoDetailsModule
     ]
 })
 export class PhotosModule {}
