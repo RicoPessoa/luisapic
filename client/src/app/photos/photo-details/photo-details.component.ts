e9e3dd103c817d3caa8f38a17523ef070d8c67ed
Ricardo Pessoa <ricardoandre.pessoa@gmail.com>
2020-11-10 15:54:53 -0300

    comments

--- client/src/app/photos/photo-details/photo-details.component.ts
+++ client/src/app/photos/photo-details/photo-details.component.ts
@@ -2,26 +2,23 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Photo } from '../photo/photo';
-import { PhotoComment } from '../photo/photo-comment';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
-    templateUrl: './photo-details.component.html',
-    styleUrls: ['./photo-details.component.css']
+    templateUrl: './photo-details.component.html'
 })
-export class PhotoDetailsComponent implements OnInit {
-    
+export class PhotoDetailsComponent implements OnInit { 
+
     photo$: Observable<Photo>;
-    comments$: Observable<PhotoComment[]>;
-    
+    photoId: number;
+
     constructor(
         private route: ActivatedRoute,
-        private photoService: PhotoService 
+        private photoService: PhotoService
     ) {}
 
     ngOnInit(): void {
-        const photoId = this.route.snapshot.params.photoId;
-        this.photo$ = this.photoService.findById(photoId);
-        this.comments$ = this.photoService.getComments(photoId);
+       this.photoId = this.route.snapshot.params.photoId;
+       this.photo$ = this.photoService.findById(this.photoId);
     }
- }
+}
